e1ec13a39f34f4be0aad3ff35c46a2aac71bb7b5
luokw7 <2316562306@qq.com>
2025-06-11 13:38:09 +0800

    修改了路由，新增了各设备对应的路由功能

--- frontend-project/src/router/index.js
+++ frontend-project/src/router/index.js
@@ -9,7 +9,10 @@ import Curtain from '@/views/devices/Curtain.vue'
 import AirConditioner from '@/views/devices/AirConditioner.vue'
 import WashingMachine from '@/views/devices/WashingMachine.vue'
 import RobotVacuum from '@/views/devices/RobotVacuum.vue'
-/*
+import Logs from '@/views/Logs.vue'
+import AddDevice from "@/views/AddDevice.vue";
+import DeleteDevice from "@/views/DeleteDevice.vue";
+
 const routes = [
   {
     path: '/',
@@ -22,21 +25,19 @@ const routes = [
   { path: '/home', name: 'Home', component: Home },
   { path: '/login', name: 'Login', component: Login },
   { path: '/register', name: 'Register', component: Register },
+  { path: '/logs', name: 'Logs', component: Logs},
   { path: '/devices', name: 'Devices', component: Devices },
   { path: '/devices/light', name: 'Light', component: Light },
+  { path: '/AddDevice' , name: "AddDevice", component: AddDevice },
+  { path: '/DeleteDevice' , name: "DeleteDevice", component: DeleteDevice },
   { path: '/devices/curtain', name: 'Curtain', component: Curtain },
   { path: '/devices/air', name: 'AirConditioner', component: AirConditioner },
   { path: '/devices/washing-machine', name: 'WashingMachine', component: WashingMachine },
   { path: '/devices/robotvacuum', name: 'RobotVacuum', component: RobotVacuum },
 
+
     // 防止路径不匹配导致页面空白
   { path: '/:catchAll(.*)', redirect: '/login' },
-]*/
-
-const routes = [
-  { path: '/', component: Home },
-  { path: '/login', component: Login },
-  { path: '/register', component: Register }
 ]
 
 const router = createRouter({
